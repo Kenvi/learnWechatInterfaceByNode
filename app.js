da31165795557a09bf5dca63a58369d5cfdda9e9
Kenvi <604113530@qq.com>
2017-01-20 17:15:20 +0800

    *

--- app.js
+++ app.js
@@ -11,6 +11,37 @@ var wechat_file = path.join(__dirname,'./config/wechat.txt');
 
 var app = new Koa();
 
+var ejs = require('ejs')
+var heredoc = require('heredoc')
+var tpl = heredoc(function(){/*
+	<!DOCTYPE html>
+	<html lang="zh-CN">
+	<head>
+		<title>猜电影</title>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">        
+		<meta name="viewport" content="initial-scale=1,maximum=1,minimum-scale=1">
+		<script type="text/javascript" src="//cdn.bootcss.com/zepto/1.2.0/zepto.js"></script>
+		<script type="text/javascript" src="http://res.wx.qq.com/open/js/jweixin-1.0.0.js"></script>
+	</head>
+	<body>
+		<h1>点击标题开始翻译</h1>	
+		<p id="title"></p>
+		<div id="poster"></p>
+	</body>
+	</html>	
+	
+*/})
+
+app.use(function *(next) {
+	if (this.url.indexOf('/movie') > -1){
+		this.body = ejs.render(tpl,{})
+		return next
+	}
+
+	yield next
+
+})
+
 app.use(wechat(config.wechat,weixin.reply));
 
 app.listen(1234);
